47ba137af15112f7c50e8ff820d223a777580491
Abdur-Rafay6229 <abdurrafay6229@gmail.com>
2026-09-26 22:33:42 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rgs-v11';
+const CACHE = 'rgs-v13';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png', './icon-192.png'];
 
 // Install event — app shell cache karo.
